3d77b95faea1aa21b82ba353cb36f285ba915fa6
Neo_Kesha <neokesha@yandex.ru>
2019-12-06 00:23:09 +0700

    Config fields added

--- NND/Serialize/Config.cs
+++ NND/Serialize/Config.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace NND.Serialize {
     public class Config {
+        [JsonProperty(PropertyName = "name")]
+        public String Name { get; set; }
+        [JsonProperty(PropertyName = "layers")]
+        public Object[] Layers { get; set; }
     }
 }
