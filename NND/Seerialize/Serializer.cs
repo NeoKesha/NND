eabc119d133c280c7d8881fe09505e83563d6fce
Neo_Kesha <neokesha@yandex.ru>
2019-12-06 00:06:29 +0700

    Serializer fields added

--- NND/Seerialize/Serializer.cs
+++ NND/Seerialize/Serializer.cs
@@ -7,6 +7,13 @@ using Newtonsoft.Json;
 
 namespace NND.Seerialize {
     public class Serializer {
-        
+        [JsonProperty(PropertyName = "class_name")]
+        public String ClassName { get; set; }
+        [JsonProperty(PropertyName = "config")]
+        public Object[] Config { get; set; }
+        [JsonProperty(PropertyName = "keras_version")]
+        public String KerasVersion { get; set; }
+        [JsonProperty(PropertyName = "backend")]
+        public String Backend { get; set; }
     }
 }
