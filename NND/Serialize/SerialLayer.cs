0e0c5fb6e6063afbe947e1759c17655b37df7da0
Neo_Kesha <neokesha@yandex.ru>
2019-12-06 16:29:05 +0700

    First version of Serial Layer

--- NND/Serialize/SerialLayer.cs
+++ NND/Serialize/SerialLayer.cs
@@ -14,7 +14,30 @@ namespace NND.Serialize
         public Dictionary<String, Object> Config { get; set; }
         public SerialLayer(Model.LayerNode node) {
             ClassName = node.Base.LayerName;
-            Config = null;
+            Config = new Dictionary<string, object>();
+            foreach(var value in node.Base.Parameters) {
+                string param_key = value.Name;
+                string param_value = node.values[param_key]; 
+                switch (value.Type.ToLower()) {
+                    case "string":
+                        Config.Add(param_key, param_value);
+                        break;
+                    case "float":
+                        Config.Add(param_key, Convert.ToSingle(param_value,System.Globalization.CultureInfo.InvariantCulture));
+                        break;
+                    case "int":
+                        Config.Add(param_key, Convert.ToInt32(param_value));
+                        break;
+                    case "tuple":
+                        string[] strs = param_value.Split(',');
+                        int[] ints = new int[strs.Length];
+                        for (var i = 0; i < strs.Length; ++i) {
+                            ints[i] = Convert.ToInt32(strs[i]);
+                        }
+                        Config.Add(param_key, ints);
+                        break;
+                }
+            }
         }
     }
 }
