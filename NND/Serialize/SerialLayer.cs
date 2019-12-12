b36207fb6c07afdc557e25fa39520265c37b4dd4
Максим Галиос <galiosmax@gmail.com>
2019-12-13 00:35:10 +0700

    ND-25 Resolve one issue

--- NND/Serialize/SerialLayer.cs
+++ NND/Serialize/SerialLayer.cs
@@ -8,39 +8,48 @@ using Newtonsoft.Json;
 namespace NND.Serialize
 {
     public class SerialLayer {
+
         [JsonProperty(PropertyName = "class_name")]
         public String ClassName { get; set; }
         [JsonProperty(PropertyName = "config")]
         public Dictionary<String, Object> Config { get; set; }
+
         [NonSerialized]
         static private string inputShape = "1";
+
         [NonSerialized]
         static private bool setInput = false;
+
         [NonSerialized]
         static private string dataType = "float32";
-        public SerialLayer(Model.LayerNode node) {
+
+        public SerialLayer(Model.LayerNode node) 
+        {
             ClassName = node.Base.LayerName;
+
             Config = new Dictionary<string, object>();
-            if (ClassName == "Input") {
-                dataType = node.values["dtype"];
-                inputShape = node.values["shape"];
-                setInput = true;
-            } else {
-                if (setInput) {
+
+            if (ClassName != "Input")
+            {
+                if (setInput)
+                {
                     string[] strs = inputShape.Split(',');
                     Object[] ints = new Object[strs.Length + 1];
                     ints[0] = null;
-                    for (var i = 1; i < strs.Length + 1; ++i) {
-                        ints[i] = Convert.ToInt32(strs[i-1]);
+                    for (var i = 1; i < strs.Length + 1; ++i)
+                    {
+                        ints[i] = Convert.ToInt32(strs[i - 1]);
                     }
                     Config.Add("batch_input_shape", ints);
                     setInput = false;
                 }
                 Config.Add("dtype", dataType);
-                foreach (var value in node.Base.Parameters) {
+                foreach (var value in node.Base.Parameters)
+                {
                     string param_key = value.Name;
                     string param_value = node.values[param_key];
-                    switch (value.Type.ToLower()) {
+                    switch (value.Type.ToLower())
+                    {
                         case "string":
                             Config.Add(param_key, param_value);
                             break;
@@ -51,16 +60,27 @@ namespace NND.Serialize
                             Config.Add(param_key, Convert.ToInt32(param_value));
                             break;
                         case "tuple":
-                            string[] strs = param_value.Split(',');
-                            int[] ints = new int[strs.Length];
-                            for (var i = 0; i < strs.Length; ++i) {
-                                ints[i] = Convert.ToInt32(strs[i]);
-                            }
-                            Config.Add(param_key, ints);
+                            Config.Add(param_key, getInts(param_value.Split(',')));
                             break;
                     }
                 }
             }
+            else
+            {
+                dataType = node.values["dtype"];
+                inputShape = node.values["shape"];
+                setInput = true;
+            }
+        }
+
+        private int[] getInts(string[] strings)
+        {
+            int[] ints = new int[strings.Length];
+            for (var i = 0; i < strings.Length; ++i)
+            {
+                ints[i] = Convert.ToInt32(strings[i]);
+            }
+            return ints;
         }
     }
 }
