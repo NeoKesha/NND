dff705ef8a053fda80a42ee58bbdd6647928f081
Максим Галиос <galiosmax@gmail.com>
2019-12-13 21:16:47 +0700

    Rewrite class LayerNode in C# style and add ToString method

--- NND/Model/LayerNode.cs
+++ NND/Model/LayerNode.cs
@@ -4,19 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NND.Model {
-    public class LayerNode {
+namespace NND.Model 
+{
+    public class LayerNode 
+    {
         static UInt32 count = 0;
+
         public LayerType Base { get; private set; }
+
         public Dictionary<string, string> values;
+
         public UInt32 Id { get; private set; }
-        public LayerNode(LayerType type) {
+
+        public LayerNode(LayerType type) 
+        {
             Base = type;
             values = new Dictionary<string, string>();
             Id = ++count;
-            foreach (var p in type.Parameters) {
+            foreach (var p in type.Parameters) 
+            {
                 values.Add(p.Name, p.DefaultValue);
             }
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {Base.LayerName}";
         }
     }
 }
