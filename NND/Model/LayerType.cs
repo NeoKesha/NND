dce96c7dc8079bfff4377f3fb05318e3cd67a370
Максим Галиос <galiosmax@gmail.com>
2019-12-13 21:17:24 +0700

    Rewrite class LayerType in C# style and add ToString method

--- NND/Model/LayerType.cs
+++ NND/Model/LayerType.cs
@@ -4,19 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NND.Model {
-    public class LayerType {
+namespace NND.Model 
+{
+    public class LayerType 
+    {
         static int Layers = 0;
+
         public int LayerID { get; private set; }
+
         public string LayerName { get; private set; }
+
         public string CategoryName { get; private set; }
+
         public List<Parameter> Parameters {get; private set; }
 
-        public LayerType(string layerName, string categoryName, Parameter[] parameters) {
+        public LayerType(string layerName, string categoryName, Parameter[] parameters)
+        {
             LayerID = ++Layers;
             LayerName = layerName;
             CategoryName = categoryName;
             Parameters = new List<Parameter>(parameters);
+        }
+        public override string ToString()
+        {
+            return LayerName;
         }
     }
 }
