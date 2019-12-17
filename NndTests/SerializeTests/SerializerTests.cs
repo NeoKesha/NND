51c21761b327a4e1268a83f1ca29be3dd977aa6c
grig-ar <a.grigorovich@g.nsu.ru>
2019-12-18 02:11:23 +0700

    add SerializerTest

---
+++ NndTests/SerializeTests/SerializerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using NND.Model;
+using NND.Serialize;
+using NUnit.Framework;
+
+namespace NndTests.SerializeTests
+{
+    [TestFixture]
+    internal class SerializerTests
+    {
+        [Test]
+        public void SerializeTest()
+        {
+            var model = new Model();
+            var serializer = new Serializer(model);
+            var memoryStream = new MemoryStream();
+            var streamWriter = new StreamWriter(memoryStream);
+            serializer.Serialize(streamWriter);
+            streamWriter.Flush();
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            const string expected =
+                "{\"class_name\":\"sequential\",\"config\":{\"name\":\"sequential_1\",\"layers\":[]},\"keras_version\":\"2.2.5\",\"backend\":\"tensorflow\"}";
+            string actual;
+            using (var reader = new StreamReader(memoryStream))
+            {
+                actual = reader.ReadLine();
+            }
+
+            Assert.True(expected.Equals(actual, StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+}
