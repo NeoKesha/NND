8302f79e4947d94024fdebf07ac6ec010e0ac75b
Neo_Kesha <neokesha@yandex.ru>
2019-12-18 02:59:42 +0700

    MaxPooling3D added

--- NND/Model/Model.cs
+++ NND/Model/Model.cs
@@ -134,6 +134,12 @@ namespace NND.Model
                                     new Parameter("padding", "String", "same" , new string[] { "same", "valid" })
                 }
                 ));
+            LayerTypes.Add(new LayerType("MaxPooling3D", "Convolutional",
+                new Parameter[]{    new Parameter("pool_size", "Tuple", "2,2,2", new string[]{ }),
+                                    new Parameter("strides", "Tuple", "1, 1, 1", new string[]{ }),
+                                    new Parameter("padding", "String", "same" , new string[] { "same", "valid" })
+                }
+                ));
         }
 
         public LayerType[] GetLayerTypes() { return LayerTypes.ToArray(); }
