4dea14648fcc051a892555c3b1d791ce03e7afe7
grig-ar <a.grigorovich@g.nsu.ru>
2019-12-18 16:19:11 +0700

    add AddNodeTest

---
+++ NndTests/ModelTests/ModelTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NND.Model;
+using NUnit.Framework;
+
+namespace NndTests.ModelTests
+{
+    [TestFixture]
+    internal class ModelTests
+    {
+        [Test]
+        public void AddNodeTest()
+        {
+            var model = new Model();
+
+            Assert.NotNull(model.GetLayerNodes());
+            Assert.That(model.GetLayerNodes()?.Length == 0);
+
+            var layerType = new LayerType("Dropout", "Core",
+                new[]
+                {
+                    new Parameter("rate", "Float", "1.0", Array.Empty<string>())
+                }
+            );
+            model.AddNode(layerType);
+            Assert.That(model.GetLayerNodes()?.Length == 1);
+        }
+    }
+}
