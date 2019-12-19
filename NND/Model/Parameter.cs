f561fe85ad35d0f2867de697e1775a2bbb7a03f1
grig-ar <a.grigorovich@g.nsu.ru>
2019-12-19 20:38:37 +0700

    fixed Parameter

--- NND/Model/Parameter.cs
+++ NND/Model/Parameter.cs
@@ -1,34 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace NND.Model
 {
     public class Parameter
     {
-        public String Name { get; private set; }
-        
-        public String Type { get; private set; }
-        
-        public String DefaultValue { get; private set; }
-        
-        private List<String> options;
-        
-        public Parameter(String name, String type, string[] options)
+        public string Name { get; }
+
+        public string Type { get; }
+
+        public string DefaultValue { get; }
+
+        private readonly List<string> _options;
+
+        public Parameter(string name, string type, string[] options)
         {
             Name = name;
             Type = type;
-            this.options = new List<string>(options);
+            _options = new List<string>(options);
             DefaultValue = "";
         }
-        
-        public Parameter(String name, String type, String defaultVal, string[] options) : this(name, type, options)
+
+        public Parameter(string name, string type, string defaultVal, string[] options) : this(name, type, options)
         {
             DefaultValue = defaultVal;
         }
-        
-        public string[] GetOptions() { return options.ToArray(); }
+
+        public string[] GetOptions()
+        {
+            return _options.ToArray();
+        }
     }
 }
