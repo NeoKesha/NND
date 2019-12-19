bb0238767705f223b6a4154b9de355e73bdfd31e
grig-ar <a.grigorovich@g.nsu.ru>
2019-12-19 21:52:50 +0700

    add annotations to Deserializer

--- NND/Serialize/Deserializer.cs
+++ NND/Serialize/Deserializer.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.IO;
+using System.Linq;
+using GuardUtils;
+using JetBrains.Annotations;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using NND.Model;
@@ -7,18 +11,28 @@ namespace NND.Serialize
 {
     public class Deserializer
     {
-        public void Deserialize(System.IO.StreamReader reader, StaticModel staticModel)
+        public void Deserialize([NotNull] StreamReader reader, [NotNull] StaticModel staticModel)
         {
+            ThrowIf.Variable.IsNull(reader, nameof(reader));
+            ThrowIf.Variable.IsNull(staticModel, nameof(staticModel));
+
             var serializer = JsonConvert.DeserializeObject<Serializer>(reader.ReadToEnd());
+
+            ThrowIf.Variable.IsNull(serializer, nameof(serializer));
+            ThrowIf.Variable.IsNull(serializer.Config, nameof(serializer.Config));
+            ThrowIf.Variable.IsNull(serializer.Config.Layers, nameof(serializer.Config.Layers));
+
             var types = staticModel.GetLayerTypesLink();
             staticModel.GetLayerNodesLink().Clear();
-            var batch_size = "";
-            var dtype = "";
-            var has_input = false;
+            var batchSize = "";
+            var dType = "";
+            var hasInput = false;
             foreach (var layer in serializer.Config.Layers)
             {
                 staticModel.AddNode(types.FirstOrDefault(t => t.LayerName == layer.ClassName));
                 var node = staticModel.GetLayerNodesLink().Last();
+                ThrowIf.Variable.IsNull(node, nameof(node));
+                ThrowIf.Variable.IsNull(layer.Config, nameof(layer.Config));
                 foreach (var param in layer.Config)
                 {
                     string str;
@@ -28,32 +42,34 @@ namespace NND.Serialize
                     }
                     else
                     {
+                        ThrowIf.Variable.IsNull(param.Value, nameof(param.Value));
                         str = param.Value.ToString();
                     }
 
                     switch (param.Key)
                     {
                         case "batch_input_shape":
-                            batch_size = str;
+                            batchSize = str;
                             break;
                         case "dtype":
-                            dtype = str;
+                            dType = str;
                             break;
                         default:
                             node.Values[param.Key] = str;
                             break;
                     }
 
-                    if (string.IsNullOrEmpty(dtype) || string.IsNullOrEmpty(batch_size) || has_input)
+                    if (string.IsNullOrEmpty(dType) || string.IsNullOrEmpty(batchSize) || hasInput)
                     {
                         continue;
                     }
 
                     staticModel.AddNode(types.FirstOrDefault(t => t.LayerName == "Input"), 0);
                     var input = staticModel.GetLayerNodesLink().First();
-                    input.Values["dtype"] = dtype;
-                    input.Values["batch_input_shape"] = batch_size;
-                    has_input = true;
+                    ThrowIf.Variable.IsNull(input, nameof(input));
+                    input.Values["dtype"] = dType;
+                    input.Values["batch_input_shape"] = batchSize;
+                    hasInput = true;
                 }
             }
         }
